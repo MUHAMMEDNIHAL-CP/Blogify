3362c7d51247bca5d510b789a52082238b70c714
Muhammed Nihal <149577575+MUHAMMEDNIHAL-CP@users.noreply.github.com>
2025-04-30 17:52:47 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = "https://a65202b3-2a1e-4215-9445-05d5aabc49aa-dev.e1-us-east-azure.choreoapis.dev/blogify/backend/v1.0"
+const apiUrl = "/choreo-apis/blogify/backend/v1"
 
 const API = axios.create({
   baseURL: 'http://localhost:8000/api/' ? 'http://localhost:8000/api/' : apiUrl,
